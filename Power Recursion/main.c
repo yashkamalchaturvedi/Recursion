6a8d325f211d1de2432463bf4e6413326dd43145
Yash <yashoka59@gmail.com>
2021-01-09 00:12:50 +0530

    initial commit

---
+++ Power Recursion/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int pow(int m,int n)
+{
+    if(n==0)
+        return 1;
+    return pow(m,n-1)*m;
+}
+int main()
+{
+    int r = pow(2,9);
+    printf("%d",r);
+    return 0;
+}
